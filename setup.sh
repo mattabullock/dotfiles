bc6df86cbc4298d120c5e059b6a82102eed0a9b1
Matt Bullock <mattabullock@gmail.com>
2023-01-18 16:27:02 -0600

    change setup and inputrc

--- setup.sh
+++ setup.sh
@@ -8,13 +8,7 @@ git clone https://github.com/vim/vim.git
 cd vim/src
 make install
 
-sudo apt -y install ctags
-
-# install golang
-curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz | sudo tar -C /usr/local -zx
+sudo apt -y install universal-ctags
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
-
-cd ~/.vim/plugged/YouCompleteMe
-./install.py --clang-completer --gocode-completer
